0414eb703a15bd4e2592e0f574e1e49a48732078
stephTchembeu <stephane.tchembeu@aims-cameroon.org>
2025-02-26 14:29:36 +0100

    modify the theme

--- assets/jsfile.js
+++ assets/jsfile.js
@@ -1,16 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const menuButton = document.getElementById("menu-btn");
-  const menuLinks = document.querySelector(".applis");
-
-  menuButton.addEventListener("click", function () {
-    menuLinks.style.display = "flex"; // Show the menu when clicked
+    const menuButton = document.getElementById("menu-btn");
+    const menuLinks = document.querySelector(".applis");
+  
+    menuButton.addEventListener("click", function () {
+      if (menuLinks.style.display === "flex") {
+        menuLinks.style.display = "none";
+      } else {
+        menuLinks.style.display = "flex";
+      }
+    });
   });
-  // Keep menu open when mouse is over it
-  menuLinks.addEventListener("mouseenter", function () {
-    menuLinks.style.display = "flex";
-  });
-  // Hide menu when mouse leaves the menu area
-  menuLinks.addEventListener("mouseleave", function () {
-    menuLinks.style.display = "none";
-  });
-});
+  
